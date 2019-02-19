a69ea126031f548361aa93609124e8cb1dfbdc71
Eric Lu <justswim@gmail.com>
2019-02-19 11:55:18 -0800

    added yarn start commands

--- server/index.py
+++ server/index.py
@@ -2,14 +2,21 @@
 This is a simple flask server 
 that handles requests from the frontend
 """
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, send_from_directory
 
+client_folder = '../client/build/'
+app = Flask(__name__, static_folder=client_folder)
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+  if path != "" and os.path.exists(client_folder + path):
+    return send_from_directory(client_folder, path)
+  else:
+    return send_from_directory(client_folder, 'index.html')
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(use_reloader=True, port=5000, threaded=True)
